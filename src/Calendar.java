680e250d5daa77b53f92334d778c849d8e525052
user <user@192.168.35.60>
2018-02-23 19:17:56 +0900

    두 수의 합을 구하는 프로그램(캘린더)

---
+++ src/Calendar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("두 수를 입력하세요.");
+		
+		String num = scanner.nextLine();
+		String[] nums = num.split(" ");
+		
+		int first = Integer.parseInt(nums[0]);
+		int second = Integer.parseInt(nums[1]);
+		
+		System.out.println("두 수의 합은 " + (first+second) + "입니다.");
+
+
+	}
+
+}
